481a7cc59eb676489635fa4d870acd0f4d9bafb6
csh <csh@CriusdeiMac.local>
2016-04-25 10:23:07 +0800

    tijiao

---
+++ Example/Pods/Target Support Files/XDJVendor/XDJVendor-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_XDJVendor : NSObject
+@end
+@implementation PodsDummy_XDJVendor
+@end
---
+++ Example/Pods/Target Support Files/XDJVendor/XDJVendor-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double XDJVendorVersionNumber;
+FOUNDATION_EXPORT const unsigned char XDJVendorVersionString[];
+
